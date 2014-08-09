a88d3d5a56f9c228e8647adac61681e144038553
Matthew Powers <matthewkevinpowers@gmail.com>
2014-08-09 00:16:37 -0400

    Cannot get the tie functio working, think I am too tired.

--- play.js
+++ play.js
@@ -3,6 +3,8 @@ function Play () {
 
   this.player_o = new Player("O");
 
+  this.players = [this.player_x, this.player_o];
+
   this.board = new Board();
 
   this.winning_positions = function () {
@@ -22,16 +24,32 @@ function Play () {
     }
   }
 
-  this.players = [this.player_x, this.player_o];
+  this.tie = function () {
+    var arr = this.board;
+    for (var i = 0, l = arr.length; i < l; i ++) {
+      if (v === undefined) { return false };
+    }
+    return true;
+  }
+
+  this.other_player = function (current_player) {
+    var arr = this.players;
+    for (var i = 0, l = arr.length; i < l; i ++) {
+      var v = arr[i];
+      if (v != current_player) {
+        return v;
+      }
+    }
+  }
 
   this.random_player = this.players[Math.floor(Math.random() * this.players.length)];
 
-  this.start_game = function () {
+  this.console_game = function () {
     console.log("Let's play tic-tac-toe!");
     var current_player = this.random_player;
     console.log("Player " + current_player.color + " was randomly selected to go first");
     while (true) {
-      var index = prompt("Enter your move");
+      var index = prompt("Enter your move Player " + current_player.color);
       this.board.set(index, current_player.color);
       if (this.winner() == "X") {
         console.log("Player X is the winner");
@@ -41,10 +59,12 @@ function Play () {
         console.log("Player O is the winner");
         break
       }
+      else if (this.tie()) {
+        console.log("The game ended in a tie");
+        break
+      }
+      current_player = this.other_player(current_player);
     }
   }
-}
 
-p = new Play();
-console.log(p.winning_positions());
-p.start_game();
+}
